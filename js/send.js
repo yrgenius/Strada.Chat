63fa62b3482010652ec79a5a79bbb668e6f9f8fa
yrgenius <yrgenius80@gmail.com>
2023-05-04 21:22:08 +0900

    feat: add user self-message

--- js/send.js
+++ js/send.js
@@ -1,13 +1,33 @@
 const FORM = document.querySelector('.send__form');
 const INPUT = document.querySelector('.input__button');
 const BUTTON = document.querySelector('.button__send');
+const CHAT = document.querySelector('.chat');
+const MESSAGE = document.querySelector('.template');
 
-FORM.addEventListener('change', formHandler);
+INPUT.addEventListener('change', formHandler);
+BUTTON.addEventListener('click', formHandler);
 
 
 function formHandler(event) {
-    console.log('ok');
+    event.preventDefault();
+
+    //TODO: разобраться со всплытием событий, события срабатывают дважды
+    console.log(event.target); // вот их инициализаторы
+
+    if (INPUT.value.length > 0) {
+        let newMessage = document.createElement('div');
+        newMessage.classList.add('message');
+        newMessage.classList.add('message__user');
+        newMessage.append(MESSAGE.content.cloneNode(1));
+        newMessage.querySelector('.template__message').textContent = INPUT.value;
+        CHAT.append(newMessage);
+    }
+
+    INPUT.value = '';
 }
 
+function validationMessage(text) {
+
+}
 
 export { FORM }
